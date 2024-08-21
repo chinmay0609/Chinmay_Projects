b268fac8ca6a634ea189705ffe6734f9a488a581
Chinmay Kote <ck091297@gmail.com>
2024-08-21 18:36:26 +0530

    Ok

--- SampleGitProject/src/com/integra/test/Test.java
+++ SampleGitProject/src/com/integra/test/Test.java
@@ -1,5 +1,10 @@
 package com.integra.test;
 
 public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Test Integra");
+	}
 
 }
---
+++ SampleGitProject/src/com/integra/test/TestIn.java
@@ -0,0 +1,5 @@
+package com.integra.test;
+
+public class TestIn {
+
+}
