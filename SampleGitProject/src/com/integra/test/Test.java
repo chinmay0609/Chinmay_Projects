ce309bcea29be8c60f6c5d7e15d1e90d98387194
chinmay0609 <71871319+chinmay0609@users.noreply.github.com>
2024-08-21 18:48:48 +0530

    Adding fixes

--- SampleGitProject/src/com/integra/test/Test.java
+++ SampleGitProject/src/com/integra/test/Test.java
@@ -5,6 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Test Integra");
+System.out.println("Test Integra");
+System.out.println("Test Integra");
+System.out.println("Test Integra");
+System.out.println("Test Integra");
 
 		System.out.println("Test Integra");
 	}
